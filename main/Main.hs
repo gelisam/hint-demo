6eabba1de888768f5a0fc876ccdc634628f6506b
Samuel Gélineau <gelisam+github@gmail.com>
2014-10-22 23:55:22 -0400

    the hello world of interpreters
    interpret the string "2+2" as an Int expression.

--- main/Main.hs
+++ main/Main.hs
@@ -1,8 +1,17 @@
 module Main where
 
+import Language.Haskell.Interpreter
+
 import Showable
 
 
 main :: IO ()
 main = do
     print (showIt (SomeShowable 1.0))
+    r <- runInterpreter $ do
+      setImports ["Prelude"]
+      n <- interpret "2+2" (as :: Int)
+      lift $ print n
+    case r of
+      Left err -> print err
+      Right () -> return ()
