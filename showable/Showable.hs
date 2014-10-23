107577ef8daf41d7612b14e8888ec1d9949d0afa
Samuel Gélineau <gelisam+github@gmail.com>
2014-10-23 00:08:16 -0400

    Showable needs to be Typeable
    otherwise the value can't be returned by the expression interpreted by
    hint

--- showable/Showable.hs
+++ showable/Showable.hs
@@ -1,9 +1,12 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DeriveDataTypeable, GADTs #-}
 module Showable where
+
+import Data.Typeable
 
 
 data SomeShowable where
   SomeShowable :: Show a => a -> SomeShowable
+  deriving Typeable
 
 showIt :: SomeShowable -> String
 showIt (SomeShowable x) = show x
